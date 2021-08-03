76695cda89ee287c1fd1c98d73ca0a7db259ccc3
= <jbloom520@gmail.com>
2021-08-03 04:21:52 -0600

    began work on the work on the EtherFrame class

--- machina.py
+++ machina.py
@@ -1,3 +1,12 @@
 # imports
 from binascii import crc32
 
+class EtherFrame(object):
+    """
+    a class to to work with a basic 802.3 ethernet frame.
+    """
+    # frame constants
+    ETHER_TYPE = b"\x88\x70"
+
+    def __init__(self):
+        # constants
